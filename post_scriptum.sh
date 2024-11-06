8b9618a793a133e423ccb0dddb6d93a1c9653a29
Hebus79 <frederic.southon@gmail.com>
2024-11-06 10:18:45 +0100

    Update post_scriptum.sh

---
+++ post_scriptum.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Déclaration des variables
+
+adresse_ip=""
+nom_utilisateur=""
+
+# Fonctions
+
+
+function creation_compte_utilisateur() {
+
+
+{
+
+
+function changement_mot_de_passe_utilisateur() {
+
+
+{
+
+function Suppression_de_compte_utilisateur_local() {
+
+
+{
+
+
+function Ajout_utilisateur_a_un_groupe_administrateur() {
+
+
+{
+
+
+
+function XXXXXXXX() {
+
+
+{
