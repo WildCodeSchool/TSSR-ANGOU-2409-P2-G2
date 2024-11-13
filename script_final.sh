296e5d95f1c8562cbfed1bfd410e2a72000f41a8
tristan14062 <tristanbouille@gmail.com>
2024-11-13 14:58:58 +0100

    Update script_final.sh

--- script_final.sh
+++ script_final.sh
@@ -1,4 +1,347 @@
 #!/bin/bash
+#Menu information utilitsateur 
+
+menu_information_utilisateur()
+{ 
+
+echo "Menu information utilisateur" 
+echo "1) Droits/Permissions de l'utilisateur sur un dossier" 
+echo "2) Droits/Permissions de l'utilisateur sur un fichier"
+echo "3) Date de dernière connexion d'un utilisateur"
+echo "4) Date de dernière modification du mot de passe de l'utilisateur" 
+echo "5) Liste des sessions ouvertes par l'utilisateur"
+echo "6) Liste des utilisateurs locaux"
+echo "r) Retour au menu Précédent"
+echo "x) Retour au Menu Principal"
+read -p "Quel est votre choix ?" choix_information_utilisateur 
+
+        case $choix_information_utilisateur in 
+
+        1) echo "Droits/Permissions de l'utilisateur sur un dossier" 
+        read -p "Quel dossier vous séléctionner ?" $directory      
+        getfacl $directory | grep user;;
+
+        2) echo "Droits/Permissions de l'utilisateur sur un fichier"
+        read -p "Quel fichier voulez vous sélectionner ?" $file
+        getfacl $file | grep user;;
+
+        3) echo "Date de dernière connexion d'un utilisateur"
+        last $nom_utilisateur;;
+
+        4) echo "Date de dernière modification du mot de passe de l'utilisateur"
+        passwd $nom_utilisateur -S;;
+
+        5) echo "Liste des sessions ouvertes par l'utilisateur"
+        w;;
+
+        6) echo "Liste des utilisateurs locaux"
+        cut -d: -f1 /etc/passwd;;
+
+        r) echo "Retour au Menu Précédent"
+           menu_information;;
+
+        x) echo "Retour au Menu Principal"
+           menu_principal;;
+           
+esac
+}
+
+#menu information système 
+
+menu_information_systeme() 
+{
+
+echo "Menu Information Système"
+
+echo "1) Informations du processeur :"
+echo "2) Mémoire RAM Totale"
+echo "3) Utilisation de la RAM"
+echo "4) Utilisation du processeur"
+echo "5) Utilisation du disque"
+echo "6) La version du système d'exploitation"
+echo "7) Liste des applications installées"
+echo "r) Retour au Menu Précédent"
+echo "x) Retour au Menu Principal"
+read -p  "Quel est votre choix ?" choix_information_system
+
+case $choix_information_system in 
+
+        1)      echo "Informations du CPU ( type de processeur)"
+                lscpu;;
+
+        2)      echo "Mémoire RAM totale"
+                cat /proc/meminfo;;
+
+        3)      echo "Utilisation de la RAM" 
+                free;;
+
+        4)      echo "Utilisation du processeur"
+                top;;
+
+        5)      echo "Utilisation du disque"
+                lsblk -f;;
+
+        6)      echo "Version de l'OS :"
+                lsb_release -a ;; 
+
+        7)      echo "Liste des applications installées :"
+                sudo dpkg -l;;
+
+        r)      echo "Retour au Menu Précédent"
+                menu_information;;
+
+        x)      echo "Retour au Menu Principal" 
+                menu_principal;;
+
+esac
+}
+action_comptes_utilisateurs () {
+
+
+    # Fonctions pour le menu Action - Comptes et Utilisateurs
+
+
+creation_compte_utilisateur () {
+
+        read -p "Nom de l'utilisateur dont le compte doit être créé? " user_account
+        echo "$(date) - Création du mot de passe pour l'utilisateur $user_account" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S useradd -m $user_account"
+        echo "Mot de passe créé pour l'utilisateur $user_account"
+        sleep 2
+    }   
+
+
+changement_mot_de_passe_utilisateur () {
+
+        read -p "Nom de l'utilisateur dont le mot de passe doit être changer ? " user_passwd
+        echo "$(date) - Modification du mot de passe pour l'utilisateur $user_passwd" >> /var/log/log_evt.log
+        ssh $nom_utilisateur@$adresse_ip "sudo -S chpasswd $user_passwd" 
+        echo "Mot de passe changé pour l'utilisateur $user_passwd"
+        sleep 2
+    }
+
+suppression_de_compte_utilisateur_local () {
+
+        read -p "Nom de l'utilisateur dont le compte doit être supprimmé ? " del_user_local_account
+        echo "Suppression du compte de l'utilisateur local $del_user_local_account" >> /var/log/log_evt.log
+        ssh $nom_utilisateur@$adresse_ip  "sudo -S deluser $del_user_local_account"
+        echo "Suppression avec succès du compte pour l'utilisateur $del_user_local_account"
+        sleep 2
+
+    }
+
+
+ajout_utilisateur_au_groupe_administrateur () {
+
+        read -p "Nom de l'utilisateur qui doit être ajouté au groupe administrateur ? " add_user_to_admin
+        echo "Ajout de l'utilisateur $add_user_to_admin au groupe administrateur" >l> /var/log/og_evt.log
+        ssh $nom_utilisateur@$adresse_ip  "sudo -S usermod -aG sudo "$add_user_to_admin""
+        echo "Ajout avec succès de l'utilisateur $add_user_to_admin au groupe administrateur"
+        sleep 2
+
+    }
+
+
+
+ajout_utilisateur_a_un_groupe_local () {
+        
+        read -p "Nom de l'utilisateur qui doit être ajouté au groupe utilisateur local ? " add_user_to_local_users
+        echo "Ajout de l'utilisateur $add_user_to_local_users au groupe local" >l> /var/log/og_evt.log
+        ssh $nom_utilisateur@$adresse_ip  "sudo -S usermod -aG users "$add_user_to_local_users""
+        echo "Ajout avec succès de l'utilisateur $add_user_to_local_users au groupe utilisateur local"
+        sleep 2
+    }
+
+sortie_utilisateur_a_un_groupe_local () {
+
+        read -p "Nom de l'utilisateur qui doit sortir du groupe utilisateur local ? " del_user_to_local_users
+        echo "sortie de l'utilisateur $del_user_to_local_users au groupe local" >l> /var/log/og_evt.log
+        ssh $nom_utilisateur@$adresse_ip  "sudo -S deluser "$del_user_to_local_users" users"
+        echo "Suppression avec succès de l'utilisateur $del_user_to_local_users du groupe utilisateur local"
+        sleep 2
+
+    }
+
+    # Menu action comptes et utilisateurs
+
+
+    while true; do
+    clear
+    echo "Menu Action comptes et utilisateurs :"
+    echo "----------------------------------"
+    echo "1- Création de compte utilisateur local"
+    echo "2- Changement de mot de passe utilisateur"
+    echo "3- Suppression de compte utilisateur local"
+    echo "4- Ajout utilisateur à un groupe administrateur"
+    echo "5- Ajout utilisateur à un groupe local"
+    echo "6- Sortie utilisateur de groupe local"
+    echo "r- Retour au menu précédent"
+    echo "x- Retour au menu principal"
+    read -p "Votre choix ? :" repcmu
+
+        case $repcmu in
+            1) creation_compte_utilisateur ;;
+            2) changement_mot_de_passe_utilisateur ;;
+            3) suppression_de_compte_utilisateur_local ;;
+            4) ajout_utilisateur_au_groupe_administrateur;;
+            5) ajout_utilisateur_a_un_groupe_local ;;
+            6) sortie_utilisateur_a_un_groupe_local ;;            
+            r) echo "Retour au menu précédent" ;;
+            x) echo "Retour au menu principal" ; menu_principal ;;
+            *) echo "Erreur choix non valide merci de modifier votre choix" ;; 
+            
+            
+        esac
+
+        sleep 2
+
+    done
+
+
+}
+
+
+
+# Menu et fonctions pour : Action - Système
+
+
+action_systeme() {
+
+
+
+    # Fonctions pour le menu Action - Système
+
+
+creation_repertoire () {
+
+        read -p "Nom du repertoire à créer (En Chemin absolu) ? " mkdir_name
+        echo "$(date +"%d/%m/%Y à %Hh%M") - $USER - Création du repertoire $mkdir_name" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S mkdir -v $mkdir_name"
+        echo "Repertoire $mkdir_name créé "
+        sleep 2
+    }
+
+
+suppression_repertoire () {
+
+        read -p "Nom du repertoire à supprimer (En Chemin absolu) ? " del_dir_name
+        echo "$(date) - Supression du repertoire $del_dir_name" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S rm -r -v $del_dir_name"
+        # echo "Repertoire $del_dir_name supprimé "
+        sleep 2
+    }
+
+
+installation_logiciel () {
+
+        read -p "Nom du package à installer ? " install_soft
+        echo "$(date) - Installation du package $install_soft" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S apt-get install $install_soft -y"
+        echo "Package $install_soft installé "
+        sleep 2
+    }
+
+
+
+desinstallation_logiciel () {
+
+        read -p "Nom du package à installer ? " desinstall_soft
+        echo "$(date) - Desinstallation du package $desinstall_soft" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S apt remove $desinstall_soft -y"
+        echo "Package $desinstall_soft desinstallé "
+        sleep 2
+    }
+
+
+execution_script () {
+
+        read -p "Nom du script à lancer ? " exec_script
+        echo "$(date) - Execution du script $exec_script" >> /var/log/log_evt.log 
+        ssh $nom_utilisateur@$adresse_ip "sudo -yes | ./$exec_script -y"
+        echo "Script $exec_script lancé "
+        sleep 2
+    }
+
+
+verrouillage_machine () {
+
+        Echo "Verrouillage de la machine" 
+        echo "$(date) - Verrouillage de  la machine  : $adresse_ip" >> /var/log/log_evt.log
+        sleep 2 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S systemctl suspend"
+    }
+
+redemarrage_machine () {
+
+        Echo "Redémarrage de la machine" 
+        echo "$(date) - Redémarrage de la machine : $adresse_ip" >> /var/log/log_evt.log
+        sleep 2 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S reboot"
+    }
+
+
+arret_machine () {
+
+        Echo "Arrêt de la machine" 
+        echo "$(date) - Arrêt de la machine : $adresse_ip" >> /var/log/log_evt.log
+        sleep 2 
+        ssh $nom_utilisateur@$adresse_ip "sudo -S shutdown now"
+    }
+
+
+update_machine () {
+
+        Echo "Mise à jour du système de la machine" 
+        echo "$(date) - Mise à jour du système : $adresse_ip" >> /var/log/log_evt.log
+        ssh $nom_utilisateur@$adresse_ip "sudo -S apt update && apt upgrade -y"
+        echo "Système mis à jour "
+        sleep 2
+    }
+
+
+
+    # Menu système
+
+    while true; do
+    clear
+    echo "Menu Action système :"
+    echo "---------------------"
+    echo "1- Création de répertoire"
+    echo "2- Suppression de répertoire"
+    echo "3- Installation de logiciel (distribution Linux Ubuntu)"
+    echo "4- Désinstallation de logiciel (distribution Linux Ubuntu)"
+    echo "5- Execution de script sur une machine distante"
+    echo "6- Verrouillage de la machine"
+    echo "7- Redémarrage de la machine"
+    echo "8- Arrêt de la machine"
+    echo "9- Mise à jour du système de la machine"
+    echo "r- Retour au menu précédent"
+    echo "x- Retour au menu principal"
+    read -p "Votre choix ? :" reps
+
+        case $reps in
+            1) creation_repertoire ;;
+            2) suppression_repertoire ;;
+            3) installation_logiciel ;;
+            4) desinstallation_logiciel ;;
+            5) execution_script ;;
+            6) verrouillage_machine ;;            
+            7) redemarrage_machine ;;
+            8) arret_machine ;;
+            9) update_machine ;;
+            r) echo "Retour au menu précédent" ;;
+            x) echo "Retour au menu principal" ; menu_principal ;;
+            *) echo "Erreur choix non valide merci de modifier votre choix" ;; 
+            
+            
+        esac
+
+        sleep 2
+    done
+
+
+}
+
 menu_information_pare_feu(){
 echo "Menu Information Pare-Feu"
 echo "1) Statut du Pare-Feu"
@@ -223,8 +566,8 @@ menu_information(){
 echo "Menu Information"
 echo "1) Menu Information Compte / Utilisateurs"
 echo "2) Menu Information Réseaux"
-echo "3) Menu système"
-echo "4) Menu sécurité"
+echo "3) Menu Information Systeme"
+echo "4) Menu Information Pare-feu"
 echo "5) Consulter le journal des événements"
 echo "x) Retour au Menu principal"
 echo "q) Sorti Script"
@@ -234,7 +577,7 @@ case $choix_menu in
 		1)
 			echo "Ouverture Menu Information Compte / Utilisateurs"
 			echo "$USER $(date) à Sélectionner le Menu Information Compte / Utilisateur" >> /var/log/log_evt.log
-			menu_comptes_et_utilisateurs;;
+			menu_information_utilisateur;;
 
 		2)
 			echo "Ouverture Menu Réseaux"
@@ -244,7 +587,7 @@ case $choix_menu in
 		3)
 			echo "Ouverture Menu système"
 			echo "$USER $(date) à Sélectionner le Menu Système" >> /var/log/log_evt.log
-			menu_system;;
+			menu_information_systeme;;
 
 		4)
 			echo "Ouverture Menu sécurité"
@@ -278,7 +621,7 @@ menu_action () {
 	echo "Menu Action
 	
 	1) Menu Comptes et Utilisateurs
-	2) Menu Système
+	2) Menu Action Système
 	3) Menu Gestion du Pare-Feu 
 	x) Retour au menu Principal
 	"
@@ -286,9 +629,9 @@ menu_action () {
 	
 	case $choix in
 		1)
-			menu_comptes_et_utilisateurs;;
+			action_comptes_utilisateurs;;
 		2)
-			menu_system;;
+			action_systeme;;
 		3)
 			menu_gestion_parefeu;;
 		x)
